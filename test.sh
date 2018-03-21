45e47dfb16b4669aa15e73a8984fec893bad37b3
Nobuhiro Kuroiwa <nobu@falcon.local>
2018-03-21 18:22:51 +0900

    add traing and test shell script

---
+++ test.sh
@@ -0,0 +1,7 @@
+
+python pix2pix.py \
+  --mode test \
+  --output_dir facades_test \
+  --input_dir facades/val \
+  --checkpoint facades_train
+  
---
+++ train.sh
@@ -0,0 +1,7 @@
+
+python pix2pix.py \
+  --mode train \
+  --output_dir facades_train \
+  --max_epochs 200 \
+  --input_dir data/input/training \
+
